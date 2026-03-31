0aa08d24eb45a60c4a9885e5db7c9e74a8da309e
JoyceJames28 <jj.starlight.2893@gmail.com>
2026-03-31 11:24:42 +0000

    waypoint explorer node and package added

---
+++ src/autonomous_exploration/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'autonomous_exploration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='jj.starlight.2893@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'waypoint_explorer = autonomous_exploration.waypoint_explorer:main',
+        ],
+    },
+)
